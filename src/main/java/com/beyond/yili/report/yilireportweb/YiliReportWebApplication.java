5d726190d7f033c5ee0feab92e3e8af53348071e
vipliliping <lipingli2018@163.com>
2019-04-16 13:41:36 +0800

    环境搭建

---
+++ src/main/java/com/beyond/yili/report/yilireportweb/YiliReportWebApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.yili.report.yilireportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiliReportWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiliReportWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beyond/yili/report/yilireportweb/YiliReportWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beyond.yili.report.yilireportweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YiliReportWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
